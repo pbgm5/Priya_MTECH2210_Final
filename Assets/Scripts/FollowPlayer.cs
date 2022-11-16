2088a6cbe7aa8dd5c5cef82578a18a2eafb800a0
pbgm5 <Priya.Begum@mail.citytech.cuny.edu>
2022-11-16 17:08:52 -0500

    added

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        transform.position = new Vector3(player.position.x, transform.position.y, 0);
+    }
+}
